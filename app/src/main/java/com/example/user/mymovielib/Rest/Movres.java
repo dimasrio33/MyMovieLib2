e7d4db11d349f6ffd9eae82ce2e11fdf9e6f4dd2
Arifbudiarto <39182827+Arifbudiarto@users.noreply.github.com>
2019-01-10 15:09:56 +0700

    Add files via upload

---
+++ app/src/main/java/com/example/user/mymovielib/Rest/Movres.java
@@ -0,0 +1,412 @@
+package com.example.user.mymovielib.Rest;
+
+public class Movres {
+    /*@SerializedName("vote_count")
+    @Expose*/
+    public Integer voteCount;
+    public Integer asl ;
+    public Integer vot;
+    public Integer vid ;
+    public Integer voteCou;
+    public Integer voteCountt;
+    /*@SerializedName("id")
+    @Expose*/
+    public Integer id;
+    /*@SerializedName("video")
+    @Expose*/
+    public Boolean video;
+    /*@SerializedName("vote_average")
+    @Expose*/
+    /*private Double voteAverage;
+    *//*@SerializedName("title")
+    @Expose*//*
+    private String title;
+    *//*@SerializedName("popularity")
+    @Expose*//*
+    private Double popularity;
+    *//*@SerializedName("poster_path")
+    @Expose*//*
+    private String posterPath;
+    *//*@SerializedName("original_language")
+    @Expose*//*
+    private String originalLanguage;
+    *//*@SerializedName("original_title")
+    @Expose*//*
+    private String originalTitle;
+    *//*@SerializedName("genre_ids")
+    @Expose*//*
+    private *//*List<Integer> genreIds = null;*//*
+    *//*SerializedName("backdrop_path")
+    @Expose*//*
+
+    private Boolean adult;
+    *//*@SerializedName("overview")
+    @Expose*//*
+    private String overview;
+    *//*@SerializedName("release_date")
+    @Expose*/
+    /*private String releaseDate;
+    private String ; private String ;
+    private String ; private String ;
+    private String ; private String ;
+    private String ; private String ;
+    private String ; private String ;
+    private String ; private String ;
+    private String ; private String ;
+
+
+    public Integer getVoteCount() {
+        return voteCount;
+    }
+
+    public void setVoteCount(Integer voteCount) {
+        this.voteCount = voteCount;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Boolean getVideo() {
+        return video;
+    }
+
+    public void setVideo(Boolean video) {
+        this.video = video;
+    }
+
+    public Double getVoteAverage() {
+        return voteAverage;
+    }
+
+    public void setVoteAverage(Double voteAverage) {
+        this.voteAverage = voteAverage;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getPopularity() {
+        return popularity;
+    }
+
+    public void setPopularity(Double popularity) {
+        this.popularity = popularity;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getOriginalLanguage() {
+        return originalLanguage;
+    }
+
+    public void setOriginalLanguage(String originalLanguage) {
+        this.originalLanguage = originalLanguage;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public *//*List<Integer> getGenreIds()*//* {
+        return *//*genreIds;*//*
+    }
+
+    public void setGenreIds(*//*List<Integer> genreIds)*//* {
+        this.*//*genreIds = genreIds;*//*
+    }
+
+    public String getBackdropPath() {
+        return backdropPath;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+    public String
+    {
+        return;
+    }
+
+    public void setBackdropPath(String backdropPath) {
+        this.backdropPath = backdropPath;
+    }
+
+    public Boolean getAdult() {
+        return adult;
+    }
+
+    public void setAdult(Boolean adult) {
+        this.adult = adult;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+*/
+    public Integer vota;
+    public Integer votb;
+    public Integer votc;
+    public Integer votd;
+    public Integer votf;
+    public Integer votg;
+    public Integer voth;
+    public Integer voti;
+    public Integer votj;
+    public Integer votk;
+    public Integer votl;
+    public Integer votm;
+    public Integer votn;
+    public Integer voto;
+    public Integer votp;
+    public Integer votq;
+    public Integer votr;
+    public Integer vots;
+    public Integer vott;
+    public Integer votu;
+    public Integer votv;
+    public Integer votw;
+    public Integer votx;
+    public Integer voty;
+    public Integer votz;
+    public Integer votca;
+    public Integer votcab;
+    public Integer votcac;
+    public Integer votcad;
+    public Integer votcae;
+    public Integer votcaf;
+    public Integer votcag;
+    public Integer votcah;
+    public Integer votcai;
+    public Integer votcaj;
+    public Integer votcak;
+    public Integer votcal;
+    public Integer votcam;
+    public Integer votcan;
+    public Integer votcao;
+    public Integer votcap;
+    public Integer votcaq;
+    public Integer votcar;
+    public Integer votcas;
+    public Integer votcat;
+    public Integer votcau;
+    public Integer votcav;
+    public Integer votcaw;
+    public Integer votcax;
+    public Integer votcay;
+    public Integer votcaz;
+    public Integer votcda;
+    public Integer votcdb;
+    public Integer votcdc;
+    public Integer votcdd;
+    public Integer votcde;
+    public Integer votcdf;
+    public Integer votcdg;
+    public Integer votcdh;
+    public Integer votcdi;
+    public Integer votcdj;
+    public Integer votcdk;
+    public Integer votcdl;
+    public Integer votcdm;
+    public Integer votcdn;
+    public Integer votcdo;
+    public Integer votcdp;
+    public Integer votcdq;
+    public Integer votcdr;
+    public Integer votcds;
+    public Integer votcdt;
+    public Integer votcdu;
+    public Integer votcdv;
+    public Integer votcdw;
+    public Integer votcdx;
+    public Integer votcdy;
+    public Integer votcdz;
+    public Integer votcdza;
+    public Integer votcdzb;
+    public Integer votcdzc;
+    public Integer votcdzd;
+    public Integer votcdze;
+    public Integer votcdzf;
+    public Integer votcdzg;
+    public Integer votcdzh;
+    public Integer votcdzi;
+    public Integer votcdzj;
+    public Integer votcdzk;
+    public Integer votcdzl;
+    public Integer votcdzm;
+    public Integer votcdzn;
+    public Integer votcdzo;
+    public Integer votcdzp;
+    public Integer votcdzq;
+    public Integer votcdzr;
+    public Integer votcdzs;
+    public Integer votcdzt;
+    public Integer votcdzu;
+    public Integer votcdzv;
+    public Integer votcdzw;
+    public Integer votcdzx;
+    public Integer votcdzy;
+    public Integer votcdzz;
+    public Integer votcdzza;
+    public Integer votcdzzb;
+    public Integer votcdzzc;
+    public Integer votcdzzd;
+    public Integer votcdzze;
+
+    public Integer votcdzzf;
+    public Integer votcdzzg;
+    public Integer votcdzzh;
+    public Integer votcdzzi;
+    public Integer votcdzzj;
+    public Integer votcdzzk;
+    public Integer votcdzzl;
+    public Integer votcdzzm;
+    public Integer votcdzzn;
+    public Integer votcdzzo;
+    public Integer votcdzzp;
+    public Integer votcdzzq;
+    public Integer votcdzzr;
+    public Integer votcdzzs;
+    public Integer votcdzzt;
+    public Integer votcdzzu;
+    public Integer votcdzzv;
+    public Integer votcdzzw;
+    public Integer votcdzzx;
+    public Integer votcdzzy;
+    public Integer votcdzzz;
+
+    public Integer votde;
+    public Integer votde1;
+    public Integer votde2;
+    public Integer votde3;
+    public Integer votde4;
+    public Integer votde5;
+    public Integer votde6;
+    public Integer votde7;
+    public Integer votde8;
+    public Integer votde9;
+    public Integer votde10;
+    public Integer votde11;
+    public Integer votde12;
+    public Integer votde13;
+    public Integer votde14;
+    public Integer votde15;
+    public Integer votde16;
+    public Integer votde17;
+    public Integer votde18;
+    public Integer votde19;
+    public Integer votde20;
+    public Integer votde21;
+    public Integer votde22;
+    public Integer votde23;
+    public Integer votde24;
+    public Integer votde25;
+    public Integer votde26;
+    public Integer votde27;
+    public Integer votde28;
+    public Integer votde29;
+    public Integer votde30;
+    public Integer votde31;
+    public Integer votde32;
+    public Integer votde33;
+    public Integer votde34;
+    public Integer votde35;
+    public Integer votde36;
+    public Integer votde37;
+    public Integer votde38;
+    public Integer votde39;
+    public Integer votde40;
+    public Integer votde41;
+    public Integer votde42;
+    public Integer votde43;
+    public Integer votde44;
+    public Integer votde45;
+    public Integer votde46;
+    public Integer votde47;
+    public Integer votde48;
+    public Integer votde49;
+    public Integer votde50;
+    public Integer votde51;
+
+
+
+}
